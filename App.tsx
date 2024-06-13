e79280bc7af227196ad0f0c0f31490cd5f7088ae
kyndrawynne <knwynne@asu.edu>
2024-06-13 14:40:07 -0700

    Removes Redux test code.

--- App.tsx
+++ App.tsx
@@ -3,17 +3,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './src/store/store';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/screens/Home/Home';
 import CalendarScreen from './src/screens/Calendar/CalendarScreen';
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Home />
-        {/* <Text>
-          This is a good response.
-        </Text> */}
         <CalendarScreen />
       </View>
     </Provider>
