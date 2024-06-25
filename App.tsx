8eb4fd5fe90ba8ff7a13caa022b84963825878ee
Rukky <ochapele@asu.edu>
2024-06-24 23:57:47 -0700

    after signup/login take to calendar page

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import store from './src/redux/store';
 import AppNavigator from './src/navigation/AppNavigator';
 import SplashScreen from './src/screens/SplashScreen';
-import _layout from './src/(tabs)/_layout';
 
 
 //not sure if i can have <_layout/> here too, had error switched out navigation to test
--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -8,7 +8,28 @@ import SplashScreen from '../screens/SplashScreen';
 import SignInScreen from '../screens/SignInScreen';
 import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Habits from '../(tabs)/Habits';
+import Calendar from '../(tabs)/Calendar';
+import Create from '../(tabs)/Create';
+import Documents from '../(tabs)/Documents';
+import Profile from '../(tabs)/Profile';
+import Ionicons from '@react-native-vector-icons/ionicons';
+
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNav = () =>{
+  return(
+      <Tab.Navigator initialRouteName='Calendar'>
+          <Tab.Screen name="Habits" component={Habits} options={{ headerShown: false}} />
+          <Tab.Screen name="Calendar" component={Calendar} options={{ headerShown: false }} />
+          <Tab.Screen name="Create" component={Create} options={{ headerShown: false }} />
+          <Tab.Screen name="Documents" component={Documents} options={{ headerShown: false }} />
+          <Tab.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+      </Tab.Navigator>
+  )
+};
 
 const AppNavigator: React.FC = () => {
   return (
@@ -19,7 +40,7 @@ const AppNavigator: React.FC = () => {
       <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
       <Stack.Screen name="EmailVerification" component={EmailVerificationScreen} options={{ headerShown: false }} />
       <Stack.Screen name="ProfileSetup" component={ProfileSetupScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Dashboard" component={DashboardScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Dashboard" component={TabNav} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 };
