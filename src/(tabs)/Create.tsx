be1b0ed9924f56e5b13f2253533d7c82d7a0f731
Rukky <ochapele@asu.edu>
2024-06-26 14:12:07 -0700

    header cleanup

--- src/(tabs)/Create.tsx
+++ src/(tabs)/Create.tsx
@@ -17,7 +17,6 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: 'center',
         paddingTop: 50,
-        paddingLeft: 20
     },
     heading:{
         fontSize:30,
