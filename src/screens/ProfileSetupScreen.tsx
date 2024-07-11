33b6f58a2b4fb0527dd87ca56ea160e3623e8e13
Max Schumacher <max.ed.schumacher@gmail.com>
2024-07-11 08:30:09 -0700

    Database functions added

--- src/screens/ProfileSetupScreen.tsx
+++ src/screens/ProfileSetupScreen.tsx
@@ -18,6 +18,7 @@ const ProfileSetupScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
   const [phone, setPhone] = useState("");
   const [birthday, setBirthday] = useState("");
   const [profilePhoto, setProfilePhoto] = useState<string | null>(null);
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const dispatch = useDispatch<AppDispatch>();
 
   const serverEndpoint = configData.API_ENDPOINT;
@@ -93,6 +94,23 @@ const ProfileSetupScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
     }
   };
 
+    const formatPhoneNumber = (input: string) => {
+        const cleanedInput = input.replace(/\D/g, "");
+        let formattedInput = cleanedInput;
+        if (cleanedInput.length > 3) {
+            formattedInput = `(${cleanedInput.slice(0, 3)})`;
+            if (cleanedInput.length > 6) {
+                formattedInput += `-${cleanedInput.slice(3, 6)}-${cleanedInput.slice(
+                    6,
+                    10
+                )}`;
+            } else {
+                formattedInput += `-${cleanedInput.slice(3)}`;
+            }
+        }
+        return formattedInput;
+    };
+
 
   const formatBirthday = (input: string) => {
     const cleanedInput = input.replace(/\D/g, "");
@@ -111,108 +129,123 @@ const ProfileSetupScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
     return formattedInput;
   };
 
-  const validateForm = () => {
-    let newErrors: { [key: string]: string } = {};
-    if (!username.match(/^[a-zA-Z0-9]+$/)) {
-      newErrors.username = "Username must contain only letters and numbers";
-    } else if (username.length < 6) {
-      newErrors.username = "Username must be at least 6 characters long";
-    } else if (username.length > 30) {
-      newErrors.username = "Username must be less than 30 characters long";
-    }
-    if (phone.replace(/\D/g, "").length !== 10)
-      newErrors.phone = "Please enter a valid 10-digit phone number";
-    if (birthday.length !== 10)
-      newErrors.birthday = "Please enter a valid date in MM/DD/YYYY format";
+    const validateForm = () => {
+        let newErrors: { [key: string]: string } = {};
+        if (!username.match(/^[a-zA-Z0-9]+$/)) {
+            newErrors.username = "Username must contain only letters and numbers";
+        } else if (username.length < 6) {
+            newErrors.username = "Username must be at least 6 characters long";
+        } else if (username.length > 30) {
+            newErrors.username = "Username must be less than 30 characters long";
+        }
+        if (phone.replace(/\D/g, "").length !== 10)
+            newErrors.phone = "Please enter a valid 10-digit phone number";
+        if (birthday.length !== 10)
+            newErrors.birthday = "Please enter a valid date in MM/DD/YYYY format";
 
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
-  };
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Set Up Profile</Text>
-      <TouchableOpacity
-        style={styles.profilePhotoContainer}
-        onPress={handleProfilePhotoUpload}
-      >
-        {profilePhoto ? (
-          <Image source={{ uri: profilePhoto }} style={styles.profilePhoto} />
-        ) : (
-          <View style={styles.profilePhotoPlaceholder}>
-            <Text style={styles.profilePhotoPlaceholderText}>Upload Photo</Text>
-          </View>
-        )}
-      </TouchableOpacity>
-      <Input
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-        style={styles.input}
-      />
-      <Input
-        placeholder="Pronouns"
-        value={pronouns}
-        onChangeText={setPronouns}
-        style={styles.input}
-      />
-      <Input
-        placeholder="Phone"
-        value={phone}
-        onChangeText={setPhone}
-        keyboardType="phone-pad"
-        style={styles.input}
-      />
-      <Input
-        placeholder="Birthday"
-        value={birthday}
-        onChangeText={setBirthday}
-        style={styles.input}
-      />
-      <Button title="Save" onPress={handleProfileSetup} />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Set Up Profile</Text>
+            <TouchableOpacity
+                style={styles.profilePhotoContainer}
+                onPress={handleProfilePhotoUpload}
+            >
+                {profilePhoto ? (
+                    <Image source={{ uri: profilePhoto }} style={styles.profilePhoto} />
+                ) : (
+                    <View style={styles.profilePhotoPlaceholder}>
+                        <Text style={styles.profilePhotoPlaceholderText}>Upload Photo</Text>
+                    </View>
+                )}
+            </TouchableOpacity>
+            <Input
+                placeholder="Display Name"
+                value={username}
+                onChangeText={setUsername}
+                style={styles.input}
+            />
+            {errors.username && (
+                <Text style={styles.errorText}>{errors.username}</Text>
+            )}
+            <Input
+                placeholder="Pronouns (optional)"
+                value={pronouns}
+                onChangeText={setPronouns}
+                style={styles.input}
+            />
+            <Input
+                placeholder="Phone (xxx)-xxx-xxxx"
+                value={phone}
+                onChangeText={(text) => setPhone(formatPhoneNumber(text))}
+                keyboardType="phone-pad"
+                style={styles.input}
+            />
+            {errors.phone && <Text style={styles.errorText}>{errors.phone}</Text>}
+            <Input
+                placeholder="Birthday MM/DD/YYYY"
+                value={birthday}
+                onChangeText={(text) => setBirthday(formatBirthday(text))}
+                keyboardType="numeric"
+                style={styles.input}
+            />
+            {errors.birthday && (
+                <Text style={styles.errorText}>{errors.birthday}</Text>
+            )}
+            <Button title="Save" onPress={handleProfileSetup} />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    padding: 16,
-    backgroundColor: colors.white,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 24,
-    textAlign: "center",
-    color: colors.blue,
-    fontFamily: fonts.regular,
-  },
-  profilePhotoContainer: {
-    alignSelf: "center",
-    marginBottom: 24,
-  },
-  profilePhoto: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-  },
-  profilePhotoPlaceholder: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    backgroundColor: colors.grey,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  profilePhotoPlaceholderText: {
-    fontSize: 16,
-    color: colors.black,
-    fontFamily: fonts.regular,
-  },
-  input: {
-    marginBottom: 16,
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        padding: 16,
+        backgroundColor: colors.white,
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 24,
+        textAlign: "center",
+        color: colors.blue,
+        fontFamily: fonts.regular,
+    },
+    profilePhotoContainer: {
+        alignSelf: "center",
+        marginBottom: 24,
+    },
+    profilePhoto: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+    },
+    profilePhotoPlaceholder: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        backgroundColor: colors.grey,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    profilePhotoPlaceholderText: {
+        fontSize: 16,
+        color: colors.black,
+        fontFamily: fonts.regular,
+    },
+    input: {
+        marginBottom: 16,
+    },
+    errorText: {
+        color: "red",
+        fontSize: 12,
+        marginTop: -12,
+        marginBottom: 16,
+        fontFamily: fonts.regular,
+    },
 });
 
 export default ProfileSetupScreen;
